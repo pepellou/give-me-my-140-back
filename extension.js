037ce3dbdc9846a62cae0df22ab51801024a1053
Pepe Doval <pepellou@gmail.com>
2017-09-30 10:34:40 +0200

    Block button when limit is surpassed

--- extension.js
+++ extension.js
@@ -1,8 +1,17 @@
 $(function() {
     var updateCounter = function() {
-        var length = $('#tweet-box-global').text().length;
-        $('#twitter-extension-my-140-back').text(length);
-        $('#twitter-extension-my-140-back').css('color', length <= 140 ? 'green' : 'red');
+        var tweetLength = $('#tweet-box-global').text().length;
+        var counter = $('#twitter-extension-my-140-back');
+        var tweetButton = $('#global-tweet-dialog-dialog .tweet-button > button');
+
+        counter.text(tweetLength);
+        if (tweetLength <= 140) {
+            counter.css('color', 'green');
+            tweetButton.removeAttr("disabled");
+        } else {
+            counter.css('color', 'red');
+            tweetButton.attr("disabled", true);
+        }
     };
 
     var styles = 'color: green; font-size: 24px; z-index: 5000; height: 30px; margin-top: -30px; float: right; margin-right: 120px;';
